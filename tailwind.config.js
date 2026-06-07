1e11395a8d79394e4da7ab050bc42474af08aed5
goneraleszek2-ship-it <goneraleszek2-ship-it@users.noreply.github.com>
2026-06-07 20:13:03 +0200

    fix: Safari search input rendering, footer narrow-viewport wrap, SVG size guard
    - Add -webkit-appearance:none and hide default cancel button for
      input[type='search'] to fix cut-off search field on iOS Safari
    - Add flex-wrap to footer nav on narrow viewports to prevent overlap
    - Add SVG intrinsic size guard to prevent unstyled SVGs from expanding
    - Add tailwind.config.js (content paths include .j2 templates)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.{j2,html}',
+    './dist/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
