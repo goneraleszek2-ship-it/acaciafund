31ebbfc040cf4e2ebc60d9dff20862dcd2c27a25
Leszek Gonera <54db764b-4ee6-4629-aca2-2510049a7a22>
2026-06-18 18:22:28 +0000

    Sync remote branch at Jun 18, 2026 at 20:22PM

--- transforms-python/src/myproject/datasets/lightweight-transform.py
+++ transforms-python/src/myproject/datasets/lightweight-transform.py
@@ -1,9 +1,13 @@
-from transforms.api import transform, Input, Output
+from transforms.api import Input, Output, transform
+
 
 @transform(
     input_dataset=Input("/TierPalan-95733d/Acacia/SOURCE_DATASET_PATH"),
-    acacia_portal_clean_data=Output("/TierPalan-95733d/Acacia/acacia_portal_clean_data")
+    acacia_portal_clean_data=Output(
+        "/TierPalan-95733d/Acacia/acacia_portal_clean_data"
+    ),
 )
-def compute(input_dataset, acacia_portal_clean_data):
+def acacia_portal_clean_data(input_dataset, acacia_portal_clean_data):
+    """Clean and standardize AcaciaFund portal data"""
     df = input_dataset.polars(lazy=True)
     acacia_portal_clean_data.write_table(df)
