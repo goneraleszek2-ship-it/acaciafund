48a6b073d428d8734294e0bf4b9d8e3d88447d48
Leszek Gonera <54db764b-4ee6-4629-aca2-2510049a7a22>
2026-06-18 14:32:50 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/lightweight-transform.py
+++ transforms-python/src/myproject/datasets/lightweight-transform.py
@@ -1,7 +1,7 @@
 import polars as pl
 from transforms.api import transform, Output, LightweightOutput
 
-@transform.using(acacia_portal_clean_data=Output("/TierPalan-95733d/Acacia/acacia_portal_clean_data"))
+@transform.using(acacia_portal_clean_data=Output("acacia_portal_clean_data"))
 def compute(acacia_portal_clean_data: LightweightOutput) -> None:
     df = pl.DataFrame({"phrase": ["Hello", "World"]})
     acacia_portal_clean_data.write_table(df)
