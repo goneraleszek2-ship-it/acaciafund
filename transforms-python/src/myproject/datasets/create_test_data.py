d750167b3df2ba5751e3329f6247711088e66d84
Leszek Gonera <54db764b-4ee6-4629-aca2-2510049a7a22>
2026-06-19 08:11:21 +0000

    fixx

--- transforms-python/src/myproject/datasets/create_test_data.py
+++ transforms-python/src/myproject/datasets/create_test_data.py
@@ -4,7 +4,7 @@ Generates realistic test data for the AcaciaFund portal.
 """
 
 import pandas as pd
-from transforms.api import transform, Output
+from transforms.api import transform, Output, TransformInput
 
 # Importujemy centralną konfigurację ścieżek projektu
 from myproject.config import DatasetPaths
@@ -13,7 +13,6 @@ from myproject.config import DatasetPaths
 @transform(
     test_source=Output(DatasetPaths.SOURCE_DATASET)
 )
-from transforms.api import transform, Output, TransformInput
 def create_test_data(test_source: TransformInput):
     df = pd.DataFrame({
         "source_id": [f"article_{i:04d}" for i in range(1, 101)],
